cfd956e2589e1a36d7151c85ae3d6ad796e16c2a
ruthl7 <nemes.balazs.ferenc@students.jedlik.eu>
2023-01-19 09:08:14 +0100

    rajzolas meno

--- main.py
+++ main.py
@@ -15,8 +15,8 @@ def biztoshkilepszV2():
     subc = tkinter.Canvas(ablak, height = 780, width = 1024, background= '#ffffff', relief='flat')
     ablak.title("Exit")
     cim3 = tkinter.Label(ablak, text = 'Are you sure?', font = ('Fette UNZ Fraktur', 40), foreground = '#850505', background='#ffffff')
-    igengomb = tkinter.Button(ablak, height = 1, width= 5, text='Yes', font = ('Fette UNZ Fraktur', 25), relief='ridge' , background='#fcba03', foreground='#850505', command=kilepigen)
-    nemgomb = tkinter.Button(ablak, height = 1, width= 5, text='No', font = ('Fette UNZ Fraktur', 25), relief='ridge' , background='#fcba03', foreground='#850505', command=kilepnem)
+    igengomb = tkinter.Button(ablak, height = 1, width= 5, text='Yes', font = ('Fette UNZ Fraktur', 25), relief='ridge' , background='#fcba03', foreground='#850505', activebackground="#850505", activeforeground="#fcba03", command=kilepigen)
+    nemgomb = tkinter.Button(ablak, height = 1, width= 5, text='No', font = ('Fette UNZ Fraktur', 25), relief='ridge' , background='#fcba03', foreground='#850505', activebackground="#850505", activeforeground="#fcba03", command=kilepnem)
 
     subc.create_window(512, 110, window = cim3)
     subc.create_window(341, 250, window = igengomb)
@@ -28,9 +28,9 @@ def kilepnem():
     ablak.title('Bob\'s trip in HELL')
 cim = tkinter.Label(ablak, text = 'Bob\'s trip in', font = ('Fette UNZ Fraktur', 50), foreground = '#850505', background='#ffffff')
 cim2 = tkinter.Label(ablak, text = 'hell', font = ('Fette UNZ Fraktur', 80), foreground = '#850505', background='#ffffff')
-startgomb = tkinter.Button(height = 1, width= 12, text='Start game', font = ('Fette UNZ Fraktur', 20), relief='ridge' , background='#fcba03', foreground='#850505', command = gamestart)
-helpgomb = tkinter.Button(height = 1, width= 12, text='Help', font = ('Fette UNZ Fraktur', 20), relief='ridge' , background='#fcba03', foreground='#850505', command = segitseg)
-exitgomb = tkinter.Button(height = 1, width= 12, text='Exit', font = ('Fette UNZ Fraktur', 20), relief='ridge' , background='#fcba03', foreground='#850505', command = biztoshkilepszV2)
+startgomb = tkinter.Button(height = 1, width= 12, text='Start game', font = ('Fette UNZ Fraktur', 20), relief='ridge' , background='#fcba03', foreground='#850505', activebackground="#850505", activeforeground="#fcba03", command = gamestart)
+helpgomb = tkinter.Button(height = 1, width= 12, text='Help', font = ('Fette UNZ Fraktur', 20), relief='ridge' , background='#fcba03', foreground='#850505', activebackground="#850505", activeforeground="#fcba03", command = segitseg)
+exitgomb = tkinter.Button(height = 1, width= 12, text='Exit', font = ('Fette UNZ Fraktur', 20), relief='ridge' , background='#fcba03', foreground='#850505', activebackground="#850505", activeforeground="#fcba03", command = biztoshkilepszV2)
 
 tuzkep = Image.open('tuz2.png')
 test = ImageTk.PhotoImage(tuzkep)
@@ -42,6 +42,19 @@ focanvas.create_window(700, 130, window = cim2)
 focanvas.create_window(512, 290, window = startgomb)
 focanvas.create_window(512, 360, window = helpgomb)
 focanvas.create_window(512, 430, window = exitgomb)
+#RAJZOLÁS
+# def get_x_and_y(event):
+#     global lasx, lasy
+#     lasx, lasy = event.x, event.y
+
+# def draw_smth(event):
+#     global lasx, lasy
+#     focanvas.create_line((lasx, lasy, event.x, event.y), 
+#                       fill='red', 
+#                       width=5)
+#     lasx, lasy = event.x, event.y
+# focanvas.bind("<Button-1>", get_x_and_y)
+# focanvas.bind("<B1-Motion>", draw_smth)
 # tag_lower()
 
 focanvas.pack()
